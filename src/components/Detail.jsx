f09821d3f5a215b754bfeff8e129765c65435a47
Dvadi <david.s.vieira15@gmail.com>
2022-04-20 19:22:38 +0100

    Criação da página de detalhe para cada empresa

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function Detail() {
+  const params= useParams();
+  const [stock, setStock]= useState();
+
+  useEffect(()=>{
+
+    fetch("https://justivo.com/stockws.php?get&code="+ params.code)
+    .then(response=> response.json())
+    .then(dataReceived=> setStock(dataReceived));
+
+  }, []);
+
   return (
-    <div>Detail</div>
+    <div>
+      {stock && (
+        <section>
+          <h2>{stock.name}</h2>
+          <h3>{stock.code}</h3>
+          <div>Preco: {stock.price}€</div>
+        </section>
+      )}
+    </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 export default function Home({stocks}) {
 
   return (
@@ -7,6 +9,9 @@ export default function Home({stocks}) {
             <h2>{stock.name}</h2>
             <h3>{stock.code}</h3>
             <div>Preco: {stock.price}€</div>
+            <div>
+              <NavLink to={"/detail/" +stock.code}>Ver Detalhe</NavLink>
+            </div>
           </section>
         ))
       }
