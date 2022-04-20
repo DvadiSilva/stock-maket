f6e29a666a58fc2fda6c6fa6ac8ac3aeecd1d1b7
Dvadi <david.s.vieira15@gmail.com>
2022-04-20 19:39:14 +0100

    Criação do butão de favorito

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -11,7 +11,14 @@ export default function Detail() {
     .then(response=> response.json())
     .then(dataReceived=> setStock(dataReceived));
 
-  }, []);
+  }, [params]);
+
+  function toogleFavorite(){
+    setStock(prevState=> ({
+      ...prevState, //coloca tudo o que já estava igual para não se perder
+      isFavorite: !prevState.isFavorite   //e alterar apenas o que queremos
+    }));
+  }
 
   return (
     <div>
@@ -20,6 +27,9 @@ export default function Detail() {
           <h2>{stock.name}</h2>
           <h3>{stock.code}</h3>
           <div>Preco: {stock.price}€</div>
+          <div>
+            <button onClick={toogleFavorite}>{stock.isFavorite? "Desfavoritar":"Favoritar"}</button>
+          </div>
         </section>
       )}
     </div>
