4a401bc428d0eb116bad377cd8302a53d0391571
Dvadi <david.s.vieira15@gmail.com>
2022-04-20 20:06:44 +0100

    Adicionado state para gestão do formulário

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Create() {
 
+  const [stock, setStock]= useState({
+    name:"",
+    code:"",
+    price: 0
+  });
+
+  
+
   function handleChange(event){
     console.log("handleChange: ", event.target.value);
+    const {name, value}= event.target; //obter o name do input e o value e colocar em variáveis
+
+    setStock(prevState=> ({
+      ...prevState,
+      [name]: value //[] são necessários
+    }));
   }
 
   function handleSubmit(event){
@@ -17,19 +31,19 @@ export default function Create() {
         <div>
           <label>
             Nome
-            <input type="text" name="name" required minLength="4" maxLength="60" onChange={handleChange}/>
+            <input type="text" name="name" required minLength="4" maxLength="60" onChange={handleChange} value={stock.name}/>
           </label>
         </div>
         <div>
           <label>
             Code
-            <input type="text" name="code" required minLength="3" maxLength="3" onChange={handleChange}/>
+            <input type="text" name="code" required minLength="3" maxLength="3" onChange={handleChange} value={stock.code}/>
           </label>
         </div>
         <div>
           <label>
             Price
-            <input type="number" name="price" required minLength="1" maxLength="99999" onChange={handleChange}/>
+            <input type="number" name="price" required minLength="1" maxLength="99999" onChange={handleChange} value={stock.price}/>
           </label>
         </div>
         <div>
